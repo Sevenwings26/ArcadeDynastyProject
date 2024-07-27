d2e38dd6d5ab9c5da6561566baa60969bd8eb3b4
Sevenwings26 <iarowosola@yahoo.com>
2024-07-27 11:09:12 +0100

    Header Completed

--- frontendArcade/src/components/Navbar.jsx
+++ frontendArcade/src/components/Navbar.jsx
@@ -1,26 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logout from "./Logout";
+import logo from "../images/brand-logo/logo.png";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold">Fashion Model</div>
-        <div className="space-x-4">
-          <Link to="/" className="text-white">
-            Home
+    <header>
+      <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-2 px-4 md:p-5 text-lg text-gray-700 bg-white">
+        <div>
+          <Link to="/">
+            <img src={logo} alt="Arcade Dynasty Logo" />
           </Link>
-          <Link to="/about" className="text-white">
-            About
-          </Link>
-          <Link to="/login" className="text-white">
-            Login
-          </Link>
-          <Logout />
         </div>
-      </div>
-    </nav>
+
+        <button
+          id="menu-button"
+          className="h-6 w-6 cursor-pointer md:hidden block"
+          onClick={handleMenuToggle}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        <div
+          className={`w-full md:flex md:items-center md:w-auto ${
+            menuOpen ? "" : "hidden"
+          }`}
+          id="menu"
+        >
+          <ul className="pt-4 text-base text-gray-700 md:flex md:justify-between md:pt-0">
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400"
+                to="/Blog"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400"
+                to="/Gallery"
+              >
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400"
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400"
+                to="/Designs"
+              >
+                Designs
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400 login-signup-link"
+                to="/login"
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="md:p-3 py-2 m-2 block hover:text-purple-400 login-signup-link"
+                to="/signup"
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
